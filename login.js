680ea925c8171e3b4cd5a36788d3337d5c496d52
narcisoo1 <narcisoo1@hotmail.com>
2021-12-29 18:48:51 -0300

    atualiza login e adiciona registro

--- login.js
+++ login.js
@@ -7,8 +7,8 @@ var path = require('path');
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '',
-	database : 'bd'
+	password : '81277593',
+	database : 'mytask'
 });
 
 var app = express();
@@ -21,14 +21,22 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.get('/login', function(request, response) {
+	response.sendFile(path.join(__dirname + '/views/login.html'));
+});
+
+app.get('/registro', function(request, response) {
+	response.sendFile(path.join(__dirname + '/views/registro.html'));
 });
 
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT * FROM usuarios WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
@@ -40,6 +48,27 @@ app.post('/auth', function(request, response) {
 		});
 	} else {
 		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.post('/register', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	var name = request.body.name;
+	var email = request.body.email;
+	if (username && password) {
+		connection.query('INSERT INTO usuarios(name,username,password,email) VALUES(?,?,?,?)', [name, username, password, email], function(error, results) {
+			if (!error) {
+				console.log('User added')
+				response.redirect('/');
+			} else {
+				response.send('Error at registration!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter all credentials!');
 		response.end();
 	}
 });
