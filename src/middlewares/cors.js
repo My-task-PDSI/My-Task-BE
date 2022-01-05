24d874fd6212519cde55c8309962d6efa21e2e92
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-05 12:45:06 -0300

    add middlewares

---
+++ src/middlewares/cors.js
@@ -0,0 +1,7 @@
+function cors(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	next();
+}
+module.exports = cors;
