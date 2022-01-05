93f90b4124191b95817f2676a74abff7c39801e4
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-05 12:47:11 -0300

    rename serve.js to app.js and refatory

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+const taskRouter = require('./routers/task');
+const taskGroupRouter = require('./routers/taskGroup');
+const userRouter = require('./routers/user');
+const indexRouter = require('./routers');
+const cors = require('./middlewares/cors');
+
+const app = express();
+
+app.use(cors);
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+app.use('/', indexRouter);
+app.use('/home', indexRouter);
+app.use('/user', userRouter);
+app.use('/task', taskRouter);
+app.use('/task-groups', taskGroupRouter);
+
+app.listen(3000);
