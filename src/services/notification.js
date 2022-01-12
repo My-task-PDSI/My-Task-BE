092935584fcbf7d7b9feeb6577abcab359a567f7
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-12 19:38:41 -0300

    update notificações

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,21 +1,21 @@
 const { database, initialize } = require("../models/database");
 
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
 }
 async function start(socketArray, ms = 1000) {
-  await initialize();
-  const { Task, Notification } = database.model;
-  console.log('iniciando sistema de notificaoes...')
-  while (true) {
-	  try {
-		  
-		  const expiredTasks = await Task.getAllExpiredWithIdUser();
-		  const allNotifications = await Notification.getAll();
-		  const addTaskArray = expiredTasks.filter((task) => {
-			  return allNotifications.every(notification => notification.idTask != task.id);
+	await initialize();
+	const { Task, Notification } = database.model;
+	console.log('iniciando sistema de notificaoes...')
+	while (true) {
+		try {
+
+			const expiredTasks = await Task.getAllExpiredWithIdUser();
+			const allNotifications = await Notification.getAll();
+			const addTaskArray = expiredTasks.filter((task) => {
+				return allNotifications.every(notification => notification.idTask != task.id);
 			});
 			for (const task of addTaskArray) {
 				const newNotification = {
@@ -23,22 +23,22 @@ async function start(socketArray, ms = 1000) {
 					idUser: task.idUser,
 					message: `task "${task.title}" is expired!`
 				};
-		await Notification.insert(newNotification);
-		console.log('nova notificacao criada', newNotification);
-		await Task.setExpiredById(task.id);
-		}
-		const notificationsNotSeen = await Notification.allNotSeen();
-		notificationsNotSeen.forEach(notification => {
-			const idUser = notification.idUser;
-			if (socketArray.hasOwnProperty(idUser)) {
-				const socket = socketArray[idUser];
-				socket.emit('notification', notification);
+				await Notification.insert(newNotification);
+				console.log('nova notificacao criada', newNotification);
+				await Task.setExpiredById(task.id);
 			}
-		});
-	} catch (error) {
-		console.error("erro nas notificações: " + error)
+			const notificationsNotSeen = await Notification.allNotSeen();
+			notificationsNotSeen.forEach(notification => {
+				const idUser = notification.idUser;
+				if (socketArray.hasOwnProperty(idUser)) {
+					const socket = socketArray[idUser];
+					socket.emit('notification', notification);
+				}
+			});
+		} catch (error) {
+			console.error("erro nas notificações: " + error)
+		}
+		await sleep(ms);
 	}
-    await sleep(ms);
-  }
 };
 module.exports = { start }
